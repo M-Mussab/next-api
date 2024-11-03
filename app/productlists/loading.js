8fc241578736226336f27dfa747b1ea00bd763cc
massbie <mmassab382@gmail.com>
2024-11-03 16:00:37 +0500

    "loader before fetching"

---
+++ app/productlists/loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+    <p className="ml-4 text-xl text-blue-500">Loading...</p>
+  </div>
+  )
+}
+
+export default loading
